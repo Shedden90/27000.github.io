70a25c933ce8889ba4987bc2b87b38a6d0464d6a
Shedden90 <75747110+Shedden90@users.noreply.github.com>
2024-02-22 09:29:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="https://fonts.cdnfonts.com/css/made-tommy-outline" rel="stylesheet">
+    <link rel="shorcut icon" type="image/x-icon" href="img/favicon.ico">
+	<link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>27000 Seguridad en ti</title>
+</head>
+<body>
+    <header>
+        <nav class="header-menu">
+            <div class="logo">
+                <div>
+                    <a href="#"><img width="200" src="img/logo.png" alt="Logo 27000"></a>
+                </div>
+            </div>
+            <div class="nav-left">
+                <div class="nav">
+                    <a href="#">Servicios</a>
+                    <a href="#">Cursos</a>
+                    <a href="#">Blog</a>
+                </div>
+            </div>
+            <div class="nav-right">
+                <div class="nav-2">
+                    <a href="#">Contacto</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="main-container">
+        <div class="presentation">
+            <div class="presentation-text">
+                <p>
+                    En <span>27000</span> impulsamos la transformación digital de forma segura.
+                </p>
+                <p class="p2">
+                    Nuestros productos y servicios se basan en normas y marcos internacionales altamente
+                    recomendados por organizaciones y expertos de todo el mundo.
+                </p>
+            </div>
+        </div>
+        <section class="business-container-section">
+            <div class="business-model">
+                <h1>Modelo de negocio diferencial</h1>
+            </div>
+            <div class="business-section">
+                <div class="business-container-1">
+                    <div class="technology-container">
+                        <div class="text-container">
+                            <h3>
+                                Tecnología líder para las operaciones "Core" del negocio
+                            </h3>
+                            <p>
+                                Conocimiento profundo de los procesos clave
+                            </p>
+                            <p>
+                                Productos propios líderes
+                            </p>
+                        </div>
+                    </div>
+                    <div class="technology-image">
+                        <img src="img/tecnologia.png" alt="Imagen tecnología">
+                    </div>
+                </div>
+                <div class="business-container-2">
+                    <div class="profesionals-container">
+                        <div class="text-container">
+                            <h3>
+                                El talento de nuestros profesionales
+                            </h3>
+                            <p>
+                                Experiencia global con equipos de implantación local
+                            </p>
+                            <p>
+                                Capacidades y habilidades diferenciales
+                            </p>
+                        </div>
+                    </div>
+                    <div class="profesionals-image">
+                        <img src="img/profesionales.png" alt="Imagen profesionales">
+                    </div>
+                </div>
+                <div class="business-container-3">
+                    <div class="clients-container">
+                        <div class="text-container">
+                            <h3>
+                                Fuerte orientación al cliente
+                            </h3>
+                            <p>
+                                Optimización del modelo de prestación de servicios y de la eficiencia de las operaciones
+                            </p>
+                            <p>
+                                Cultura única de adaptación a sus necesidades.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="clients-image">
+                        <img src="img/clientes.png" alt="Imagen Clientes">
+                    </div>
+                </div>
+                <div class="business-container-4">
+                    <div class="solutions-container">
+                        <div class="text-container">
+                            <h3>
+                                Cartera de soluciones tecnológicas
+                            </h3>
+                            <p>
+                                Equilibrada y diversificada
+                            </p>
+                            <p>
+                                Conocimiento de los factores competitivos clave en diferentes sectores
+                            </p>
+                        </div>
+                    </div>
+                    <div class="solutions-image">
+                        <img src="img/soluciones.png" alt="Imagen soluciones">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-services">
+            <div class="services-container">
+                <div class="services-title">
+                    <h2>Servicios</h2>
+                </div>
+                <div class="itsecurity-container">
+                    <div class="itsecurity-image">
+                        <img width="700" src="img/sdi.png" alt="Imagen Seguridad de la Información">
+                    </div>
+                    <div class="itsecurity-text">
+                        <h3>Seguridad de la Información</h3>
+                        <p>
+                            Protegemos tu información confidencial y sus activos digitales con soluciones innovadoras
+                            y estrategias proactivas.
+                        </p>
+                        <div class="itsecurity-button">
+                            <a href="#">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="auditory-container">
+                    <div class="auditory-image">
+                        <img width="700" src="img/ads.jpg" alt="Imagen Auditoria de la Seguridad">
+                    </div>
+                    <div class="auditory-text">
+                        <h3>Auditorias e implementación de marcos</h3>
+                        <p>
+                            Cumplir con normativas y regulaciones es una ventaja competitiva y un gran diferenciador
+                            ante tu competencia.
+                        </p>
+                        <div class="auditory-button">
+                            <a href="#">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-courses">
+            <div class="courses-container">
+                <div class="courses-title">
+                    <h2>Cursos y capacitaciones</h2>
+                </div>
+                <div class="courses-container-1">
+                    <div class="courses-container-image">
+                        <img src="img/courses.png" alt="Cursos Seguridad Informática">
+                    </div>
+                    <div class="courses-text">
+                        <h3>Cursos</h3>
+                        <p>
+                            En un mundo cada vez más interconectado, la seguridad de la información es fundamental
+                        </p>
+                        <div class="courses-button">
+                            <a href="#">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="courses-container-2">
+                    <div class="certification-container-image">
+                        <img src="img/iso.jpg" alt="Certificación ISO 27001">
+                    </div>
+                    <div class="certification-text">
+                        <h3>Certificaciones</h3>
+                        <p>
+                            Obtén el reconocimiento que tu organización merece con nuestras certificaciónes
+                        </p>
+                        <div class="certification-button">
+                            <a href="#">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="courses-container-3">
+                    <div class="users-container-image">
+                        <img src="img/usuarios.jpg" alt="Concientización de usuarios">
+                    </div>
+                    <div class="users-text">
+                        <h3>Concientización de usuarios</h3>
+                        <p>
+                            Convertimos al usuario en la primera línea de defensa
+                        </p>
+                        <div class="users-button">
+                            <a href="#">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="blog"></section>
+        <section class="contact">
+            <div class="contact-text">
+                <h3>
+                    ¿Qué necesita tu empresa de nosotros?
+                </h3>
+                <p>
+                    Usa el formulario de contacto para resolver tus dudas de nuestros servicios
+                    o por cualquier asesoría necesaria.
+                </p>
+                <p>
+                    Dirección:
+                    Eva Samano Col. México #1032-5 C.P. 88796 Reynosa Tamps.
+                </p>
+                <p>
+                    Teléfono: <span class="azul"><a href="tel:+528991198888">+52 8991 19 8888</a></span>
+                </p>
+                <p>
+                    Correo: <span class="azul"><a href="mailto:hello@contacto@27000.com.mx">contacto@27000.com.mx</a></span>
+                </p>
+            </div>
+            <div class="contact-form">
+                <form action="">
+                    <div class="contact-field">
+                        <input type="text" placeholder="Nombre:" name="nombre" id="nombre" require>
+                    </div>
+                    <div class="contact-field">
+                        <input type="email" placeholder="Correo electrónico:" name="correo" id="correo" require>
+                    </div>
+                    <div class="contact-field">
+                        <input type="text" placeholder="Teléfono:" name="telefono" id="telefono" require>
+                    </div>
+                    <div class="contact-field">
+                        <textarea name="mensaje" id="mensaje" placeholder="Mensaje:"></textarea>
+                    </div>
+                    <div class="send-message">
+                        <input type="submit" value="Enviar mensaje" name="contacto" id="contacto">
+                    </div>
+                </form>
+            </div>
+        </section>
+        <footer>
+            <div class="footer-container">
+                <div class="footer-logo">
+                    <img width="700" src="img/logo-negativo.png" alt="Logo 27000">
+                </div>
+                <div class="footer-social">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                </div>
+            </div>  
+            <div class="footer-rights">
+                <p>
+                    © 2024 - 27000 Seguridad en ti. Todos los derechos reservados
+                </p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
